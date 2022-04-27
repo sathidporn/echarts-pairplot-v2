32d6d0b2d5bbbd9e230fd09246036dc78c2a7944
sathidporn <77266865+sathidporn@users.noreply.github.com>
2022-04-27 14:37:21 +0700

    Add submit button

--- src/components/SamplingPicker/index.js
+++ src/components/SamplingPicker/index.js
@@ -1,7 +1,7 @@
 // import papa from 'papaparse'
 import React, { useCallback, useState } from 'react';
 import Grid from '@mui/material/Grid'
-import { Typography } from '@mui/material';
+import { Typography, Button } from '@mui/material';
 import SelectorItem from '../SelectorItem';
 import { style } from '../../styles/style';
 const useStyles = style
@@ -19,17 +19,17 @@ const samplingList = [
 export default function SamplingPicker({ onSamplingData = () => {}}) {
   const classes = useStyles();
   let [samplingType, setSamplingType] = useState("select")
-  const handleChange = useCallback((value) => {
-    setSamplingType(value)
-    onSamplingData(value)
-  }, [setSamplingType, onSamplingData])
+
   return (
       <Grid item container xs={12} sm={12} md={12} lg={12} spacing={1}>
-        <Grid item xs={4} sm={4} md={4} lg={4} align="left">
-            <Typography className={classes.blueText}>SAMPLING TYPE :  </Typography>
+        <Grid item xs={3} sm={3} md={3} lg={3} align="left">
+            <Typography className={classes.blueText}>Sampling by  </Typography>
         </Grid>
-        <Grid item xs={8} sm={8} md={8} lg={8}>
-          <SelectorItem id={'sampling'} value={samplingType} items={samplingList} onChange={(value) => handleChange(value)}></SelectorItem>
+        <Grid item xs={5} sm={5} md={5} lg={5} align="left">
+            <SelectorItem id={'sampling'} value={samplingType} items={samplingList} onChange={(value) => setSamplingType(value)}></SelectorItem>
+        </Grid>
+        <Grid item  xs={4} sm={4} md={4} lg={4} align="left">
+            <Button className={classes.defaultButton} onClick={()=>onSamplingData(samplingType)}>Submit</Button>
         </Grid>
       </Grid>
   );
